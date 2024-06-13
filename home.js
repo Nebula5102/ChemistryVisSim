26e044bb5a3449d3004f130fe8fbfbe3cc05b0a5
Nebula5102 <nicklausb@shaw.ca>
2024-06-13 11:53:25 -0700

    just testing and playing with javascript

--- home.js
+++ home.js
@@ -1,5 +1,40 @@
+var valAnimation = false;
+
 function addCarbon() {
+	var particleBox = document.getElementById("particleBox");
 	var x = document.createElement("DIV");
 	x.setAttribute('class','circle');
-	document.body.appendChild(x);
+	particleBox.appendChild(x);
+}
+
+
+function setAnimation() {
+	if (valAnimation == false) {
+		valAnimation = true;
+		var element = document.getElementById("animateButton")
+		element.innerHTML = "Stop Animation";
+		animate();
+	} else {
+		valAnimation = false;
+		var element = document.getElementById("animateButton")
+		element.innerHTML = "Animate";
+	}
+};
+
+
+function animate() {
+	var particles = document.getElementsByClassName("circle")
+	var pos = 1;
+	for (let i = 0; i < particles.length; i++) {
+		particle = particles[i];
+		position = particle.style.top;
+		console.log(position);
+		particle.style.top = pos + position + 'px';
+		console.log(particle.style.top);
+	}
+	//	var pos = 0;
+	//	id = setInterval(frame,10);
+	//	function frame() {
+	//		
+	//	}
 }
